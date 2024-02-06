d49617231f12dc00c8fa23b174de2904d6bd4518
mkg1 <gehring.mg@gmail.com>
2024-02-06 07:08:44 -0500

    Restructure components; make teleportal parent component; add styled input

--- src/lib/components/Teleport/Teleportal.svelte
+++ src/lib/components/Teleport/Teleportal.svelte
@@ -1,6 +1,11 @@
 <script>
     import Portal from '$lib/images/teleportbutton.png';
+    import Header from '$lib/components/Header/Header.svelte'
+    import Footer from '$lib/components/Footer/Footer.svelte'
     import { takeMeSomewhere } from '$lib/utils/Destination.js';
+
+    let pagecontext = 'portal';
+    console.log("pagecontext: ", pagecontext)
     function teleport() {
         var bodyElement = document.body;
         bodyElement.classList.add("teleporting");
@@ -9,12 +14,21 @@
         setTimeout(() => {  window.location = teleportDestination; }, 2000)
     }
 </script>
-
+<Header bind:pagecontext />
 <div id="portal">
-    <button on:click={teleport}>
-        <img src={Portal} alt="a teleportation device" />
-    </button>
+{#if pagecontext == 'enterDestination'}
+    <div id="input-container">
+        <input placeholder="Enter a new destination..." />
+        <p>Enter a new teleport destination</p>
+        <p id="subtext">(must be a permanent link)</p>    
+    </div>
+{:else}
+        <button on:click={teleport}>
+            <img src={Portal} alt="a teleportation device" />
+        </button>
+{/if}
 </div>
+<Footer />
 
 <style>
     #portal {
@@ -35,5 +49,36 @@
     button {
         background: none;
         border: none;
+    }
+
+    #input-container {
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    input {
+        margin-top: 50%;
+        width: 75%;
+        background: rgb(176 236 241 / 30%);
+        border: 2px solid #aae3ff;
+        height: 30px;
+        border-radius: 8px;
+        text-align: center;
+        color: white;
+        font-size: 16px;
+    }
+
+    ::placeholder {
+        color: rgb(160, 157, 157);
+    }
+
+    p {
+        font-family: 'Bungee Hairline', sans-serif;
+        color: white;
+    }
+
+    #subtext {
+        margin: 0;
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,8 @@
 
 <script>
     import '$lib/styles/styles.css';
-    import Header from '$lib/components/Header/Header.svelte'
     import Teleportal from '$lib/components/Teleport/Teleportal.svelte'
-    import Footer from '$lib/components/Footer/Footer.svelte'
 </script>
-<Header />
 <Teleportal />
-<Footer />
 
 
